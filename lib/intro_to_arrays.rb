2e6cbdcb13f76f367afd128f516b16092317103e
Meg Galvez <meg@oakmaine.com>
2019-02-20 20:56:07 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,40 @@
+def instantiate_new_array
+ @my_new_array = []
+end
+  
+def array_with_two_elements
+  @my_two_array = [1, 2]
+end
+
+def first_element(first_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  first_element.first 
+end
+
+def third_element(third_element)
+  @taylor_swift
+  third_element[2]
+end
+
+def last_element(last_element)
+  @taylor_swift
+  last_element[-1]
+end
+
+def first_element_with_array_methods(first_country)
+  @south_east_asia
+  first_country.first
+end
+
+def last_element_with_array_methods(last_country)
+  @south_east_asia
+  last_country.last
+
+end
+
+def length_of_array(length)
+  @programming_languages
+  length.length
+end
+
+
